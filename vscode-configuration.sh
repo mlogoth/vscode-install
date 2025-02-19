8c8609ebace870f016cc7f59a04b58ff1a092289
Michalis <logothm@gmail.com>
2025-02-19 11:24:08 +0200

    update bash script

--- vscode-configuration.sh
+++ vscode-configuration.sh
@@ -1,49 +1,80 @@
-code --install-extension ms-python.python 
-code --install-extension ms-python.black-formatter 
-code --install-extension ms-vscode.cpptools 
-code --install-extension ms-iot.vscode-ros 
-code --install-extension ms-vscode.cmake-tools 
-code --install-extension twxs.cmake 
-code --install-extension ms-azuretools.vscode-docker 
-code --install-extension ms-vscode.remote-containers 
-code --install-extension ms-vscode.remote-ssh 
-code --install-extension ms-vscode.remote-explorer 
-code --install-extension ms-vscode.github 
-code --install-extension eamodio.gitlens 
-code --install-extension github.vscode-github-actions 
-code --install-extension github.copilot 
-code --install-extension github.copilot-chat 
-code --install-extension github.vscode-pull-request-github 
-code --install-extension redhat.vscode-yaml 
-code --install-extension davidanson.vscode-markdownlint 
-code --install-extension streetsidesoftware.code-spell-checker 
-code --install-extension foxundermoon.shell-format 
-code --install-extension timonwong.shellcheck 
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin 
-code --install-extension ms-vscode.makefile-tools 
-code --install-extension ms-vscode.cpptools-extension-pack 
-code --install-extension ms-vscode.hexeditor 
-code --install-extension gruntfuggly.todo-tree 
-code --install-extension tamasfe.even-better-toml 
-code --install-extension oderwat.indent-rainbow 
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack 
-code --install-extension ms-vscode.pylance 
-code --install-extension donjayamanne.githistory 
-code --install-extension zhuangtongfa.material-theme 
-code --install-extension esbenp.prettier-vscode 
-code --install-extension shardulm94.trailing-spaces 
-code --install-extension ritwickdey.liveserver 
-code --install-extension vscode-icons-team.vscode-icons 
-code --install-extension pkief.material-icon-theme 
-code --install-extension dbaeumer.vscode-eslint 
-code --install-extension aaron-bond.better-comments 
-code --install-extension njpwerner.autodocstring 
-code --install-extension mhutchie.git-graph 
-code --install-extension webfreak.debug 
-code --install-extension humao.rest-client 
-code --install-extension mechatroner.rainbow-csv 
-code --install-extension ms-toolsai.jupyter 
-code --install-extension ms-toolsai.jupyter-keymap 
-code --install-extension ms-toolsai.jupyter-renderers 
-code --install-extension ms-toolsai.vscode-jupyter-cell-tags 
-code --install-extension ms-toolsai.vscode-jupyter-slideshow
+#!/bin/bash
+
+# Function to install a VS Code extension
+install_extension() {
+    local extension=$1
+    if code --list-extensions | grep -q "$extension"; then
+        echo "Extension $extension is already installed."
+    else
+        code --install-extension "$extension"
+        if [ $? -eq 0 ]; then
+            echo "Successfully installed $extension."
+        else
+            echo "Failed to install $extension."
+        fi
+    fi
+}
+
+# Check if VS Code is installed
+if ! command -v code &> /dev/null; then
+    echo "VS Code is not installed. Please install it first."
+    exit 1
+fi
+
+# List of extensions to install
+extensions=(
+    ms-python.python
+    ms-python.black-formatter
+    ms-vscode.cpptools
+    ms-iot.vscode-ros
+    ms-vscode.cmake-tools
+    twxs.cmake
+    ms-azuretools.vscode-docker
+    ms-vscode.remote-containers
+    ms-vscode.remote-ssh
+    ms-vscode.remote-explorer
+    ms-vscode.github
+    eamodio.gitlens
+    github.vscode-github-actions
+    github.copilot
+    github.copilot-chat
+    github.vscode-pull-request-github
+    redhat.vscode-yaml
+    davidanson.vscode-markdownlint
+    streetsidesoftware.code-spell-checker
+    foxundermoon.shell-format
+    timonwong.shellcheck
+    ms-vscode.vscode-typescript-tslint-plugin
+    ms-vscode.makefile-tools
+    ms-vscode.cpptools-extension-pack
+    ms-vscode.hexeditor
+    gruntfuggly.todo-tree
+    tamasfe.even-better-toml
+    oderwat.indent-rainbow
+    ms-vscode-remote.vscode-remote-extensionpack
+    ms-vscode.pylance
+    donjayamanne.githistory
+    zhuangtongfa.material-theme
+    esbenp.prettier-vscode
+    shardulm94.trailing-spaces
+    ritwickdey.liveserver
+    vscode-icons-team.vscode-icons
+    pkief.material-icon-theme
+    dbaeumer.vscode-eslint
+    aaron-bond.better-comments
+    njpwerner.autodocstring
+    mhutchie.git-graph
+    webfreak.debug
+    humao.rest-client
+    mechatroner.rainbow-csv
+    ms-toolsai.jupyter
+    ms-toolsai.jupyter-keymap
+    ms-toolsai.jupyter-renderers
+    ms-toolsai.vscode-jupyter-cell-tags
+    ms-toolsai.vscode-jupyter-slideshow
+)
+
+# Install each extension
+for extension in "${extensions[@]}"; do
+    install_extension "$extension"
+done
